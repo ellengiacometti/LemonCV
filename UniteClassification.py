5f43b65804fd8afcd439b84a9252c464c5d79572
ellen.giacometti <ellengiacometti@accenture.com>
2020-08-08 13:28:01 -0300

    Added final label

--- UniteClassification.py
+++ UniteClassification.py
@@ -1,13 +1,17 @@
 import os
 import glob
 import pandas as pd
-dir ="C:/Users/ellen.giacometti/PycharmProjects/LiMatch/CLASSIFICACOES"
+# Setting path to read classification files
+dir ="C:/Users/ellen.giacometti/PycharmProjects/LemonCV/CLASSIFICACOES"
+# Creating columns for final file, the one with voted classification
 final_sample = []
+final_label=[]
 R = []
 L = []
 C = []
 S = []
 X = []
+# Looking up all  files inside provided path folder
 for filename in os.listdir(dir):
     print(filename)
     # Reading CSV file with labeled info
@@ -15,7 +19,7 @@ for filename in os.listdir(dir):
     # Separating file columns
     sample_column=pd.Series(file['Sample'])
     label_column=pd.Series(file['Rotulo'])
-    # Creating columns for final file, the one with voted classification
+
     for index, sample_filename in enumerate(sample_column):
         x=0
         maturity=''
@@ -25,10 +29,10 @@ for filename in os.listdir(dir):
             defect = label_column[index][1]
         else:
             x=1
-
+        # Checking if sample was already classified
         if sample_filename in final_sample:
             index_final = final_sample.index(sample_filename)
-            # Getting each attribute label
+            # Getting each attribute label and adding with previous one
             if x==1:
                 X[index_final] = X[index_final] + 1
             else:
@@ -40,6 +44,7 @@ for filename in os.listdir(dir):
                     C[index_final] = C[index_final] + 1
                 elif defect == "S":
                     S[index_final] = S[index_final] + 1
+        #If sample was not in file , create a new line and add current label
         else:
             final_sample.append(sample_filename)
 
@@ -76,9 +81,34 @@ for filename in os.listdir(dir):
                     L.append(1)
                     S.append(0)
 
+
+data=pd.DataFrame([R, L, C, S, X])
+
+for index in range(data.shape[1]):
+    if  X[index]>(R[index]+L[index]) or X[index]>(C[index]+S[index]):
+        # Drop Sample
+        final_label.append('X')
+    else:
+        if R[index]>L[index]:
+            if C[index]>=S[index]:
+                # RC
+                final_label.append('RC')
+            else:
+                # RS
+                final_label.append('RS')
+        elif L[index]>=R[index]:
+            if C[index]>=S[index]:
+                # LC
+                final_label.append('LC')
+            else:
+                #LS
+                final_label.append('LS')
+
 final_sample_name= pd.Series(final_sample)
+# Cleaning sample name (an additional sequence was introduced to serialize sample at Limatch)
 final_sample_name=final_sample_name.str.slice(start=-12)
-raw_data = {'nome': final_sample_name, 'R': R,'L': L,'C': C,'S': S,'X': X}
-df = pd.DataFrame(raw_data,columns=['nome', 'R','L','C','S','X'])
+
+raw_data = {'nome': final_sample_name, 'R': R,'L': L,'C': C,'S': S,'X': X,'Voted Label':final_label}
+df = pd.DataFrame(raw_data,columns=['Sample', 'R','L','C','S','X','Voted Label'])
 df.to_csv('VotedClassification.csv',index=False,sep=";")
 print("Arquivo Criado!")
