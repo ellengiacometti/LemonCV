f0271f0c52d6949d9aad507fbec30d3a7b19430b
ellen.giacometti <ellengiacometti@accenture.com>
2020-08-08 18:22:14 -0300

    Version for  new training

--- UniteClassification.py
+++ UniteClassification.py
@@ -22,65 +22,55 @@ for filename in os.listdir(dir):
 
     for index, sample_filename in enumerate(sample_column):
         x=0
-        maturity=''
-        defect=''
+        if filename == os.listdir(dir)[0]:
+            final_sample.append(sample_filename)
+            maturity = final_sample.__str__()[-9]
+            defect = final_sample.__str__()[-8]
+            if maturity == "R" and defect == "C":
+                X.append(0)
+                R.append(1)
+                C.append(1)
+                L.append(0)
+                S.append(0)
+
+            elif maturity == "L" and defect == "C":
+                X.append(0)
+                R.append(0)
+                C.append(1)
+                L.append(1)
+                S.append(0)
+            elif maturity == "L" and defect == "S":
+                X.append(0)
+                R.append(0)
+                C.append(0)
+                L.append(1)
+                S.append(1)
+            elif maturity == "R" and defect == "S":
+                X.append(0)
+                R.append(1)
+                C.append(0)
+                L.append(1)
+                S.append(0)
+
         if len(label_column[index]) != 1:
             maturity = label_column[index][0]
             defect = label_column[index][1]
         else:
             x=1
-        # Checking if sample was already classified
-        if sample_filename in final_sample:
-            index_final = final_sample.index(sample_filename)
-            # Getting each attribute label and adding with previous one
-            if x==1:
-                X[index_final] = X[index_final] + 1
-            else:
-                if maturity == "R":
-                    R[index_final] = R[index_final] + 1
-                elif maturity == "L":
-                    L[index_final] = L[index_final] + 1
-                if defect == "C":
-                    C[index_final] = C[index_final] + 1
-                elif defect == "S":
-                    S[index_final] = S[index_final] + 1
-        #If sample was not in file , create a new line and add current label
+
+        index_final = final_sample.index(sample_filename)
+        # Getting each attribute label and adding with previous one
+        if x==1:
+            X[index_final] = X[index_final] + 1
         else:
-            final_sample.append(sample_filename)
-
-            if x==1:
-                X.append(1)
-                R.append(0)
-                L.append(0)
-                C.append(0)
-                S.append(0)
-            else:
-                if maturity == "R" and defect == "C" :
-                    X.append(0)
-                    R.append(1)
-                    C.append(1)
-                    L.append(0)
-                    S.append(0)
-
-                elif maturity == "L" and defect == "C":
-                    X.append(0)
-                    R.append(0)
-                    C.append(1)
-                    L.append(1)
-                    S.append(0)
-                elif maturity == "L" and defect == "S":
-                    X.append(0)
-                    R.append(0)
-                    C.append(0)
-                    L.append(1)
-                    S.append(1)
-                elif maturity == "R"and defect == "S":
-                    X.append(0)
-                    R.append(1)
-                    C.append(0)
-                    L.append(1)
-                    S.append(0)
-
+            if maturity == "R":
+                R[index_final] = R[index_final] + 1
+            elif maturity == "L":
+                L[index_final] = L[index_final] + 1
+            if defect == "C":
+                C[index_final] = C[index_final] + 1
+            elif defect == "S":
+                S[index_final] = S[index_final] + 1
 
 data=pd.DataFrame([R, L, C, S, X])
 
@@ -108,7 +98,7 @@ final_sample_name= pd.Series(final_sample)
 # Cleaning sample name (an additional sequence was introduced to serialize sample at Limatch)
 final_sample_name=final_sample_name.str.slice(start=-12)
 
-raw_data = {'nome': final_sample_name, 'R': R,'L': L,'C': C,'S': S,'X': X,'Voted Label':final_label}
+raw_data = {'Sample': final_sample_name, 'R': R,'L': L,'C': C,'S': S,'X': X,'Voted Label':final_label}
 df = pd.DataFrame(raw_data,columns=['Sample', 'R','L','C','S','X','Voted Label'])
 df.to_csv('VotedClassification.csv',index=False,sep=";")
 print("Arquivo Criado!")
